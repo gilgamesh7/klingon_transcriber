e430af4f88c70d9caf4edd4f4ab914c982ac42db
gilgamesh7 <felidaesrule@yahoo.com>
2022-06-10 22:02:34 +1200

    Display input list of images

--- klingon/views.py
+++ klingon/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def klingon_transcriber(request):
-    return render(request, 'klingon/klingon.html')
+
+    image_list = ['klingon/images/a.png', 'klingon/images/b.png', 'klingon/images/ch.png']
+
+    form_display = {'images': image_list}
+    
+    return render(request, 'klingon/klingon.html', form_display)
